b27eeb023da1dba6b9cfd73e271685b9ad715f69
yanglet <y2yone023@naver.com>
2024-03-05 09:39:27 +0900

    테스트 코드 작성

--- src/test/java/com/example/icon/IconTest.java
+++ src/test/java/com/example/icon/IconTest.java
@@ -8,12 +8,14 @@ import foundation.icon.icx.crypto.ECDSASignature;
 import foundation.icon.icx.data.Address;
 import foundation.icon.icx.data.Bytes;
 import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
 import org.bouncycastle.jcajce.provider.digest.SHA3;
 import org.bouncycastle.util.BigIntegers;
 import org.bouncycastle.util.encoders.Base64;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigInteger;
+import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
 
 
@@ -22,43 +24,27 @@ public class IconTest {
 
     private final String serializedTransaction
             = "icx_sendTransaction.from.hx8a2c51300334d7791cf8377ae42b721f537a4e01.nid.0x1.nonce.0x1.stepLimit.0x186a0.timestamp.0x6123ef33545b8.to.hxb678bd32e86308fdc05bf44fe005c0c935dbaba2.value.0x3e8.version.0x3";
-    private final String Signature = "nZPeyCEWbuWTLIOlQYSr4e8dUvhyURsGhi33izsNVdoUQCsJ4GY7yHf7TJB2z+hFg1SzcYMKQR2nmEQBXno86gE=";
-    private final String password = "icxTest";
     private final BigInteger key = new BigInteger("64186588625296304500275520245867802253924063285282458200975891251650071368419");
 
-    /**
-     * icx_sendTransaction.from.hx8a2c51300334d7791cf8377ae42b721f537a4e01.nid.0x1.stepLimit.0x186a0.timestamp.0x6123ef33545b8.to.hxb678bd32e86308fdc05bf44fe005c0c935dbaba2.value.0x3e8.version.0x3
-     * 1708911437891000
-     * nZPeyCEWbuWTLIOlQYSr4e8dUvhyURsGhi33izsNVdoUQCsJ4GY7yHf7TJB2z+hFg1SzcYMKQR2nmEQBXno86gE=
-     */
-
     @Test
-    public void test() {
-        Bytes privateKey = new Bytes(BigIntegers.asUnsignedByteArray(32, key));
-        log.info("privateKey = {}", privateKey);
+    public void sign() {
+        final String sdkSignature = getSdkSignature();
+        final String signature = getSignature();
 
-        ECDSASignature ecdsaSignature = new ECDSASignature(privateKey);
-        byte[] data = new SHA3.Digest256().digest(serializedTransaction.getBytes(StandardCharsets.UTF_8));
-        BigInteger[] sig = ecdsaSignature.generateSignature(data);
-        byte[] signatureBytes = ecdsaSignature.recoverableSerialize(sig, data);
-        String signature = Base64.toBase64String(signatureBytes);
-        log.info("signature = {}", signature);
-        // 2DL341dGihRSQrVPDflLQHQnTvxeQgsjl62Y35LVthdpmyJjXN0/QDW0D+QP4vOqExZucDQS7rcYmOh2kEGuqwA=
+        Assertions.assertThat(sdkSignature).isEqualTo(signature);
     }
 
-    @Test
-    public void sdk() {
-        Bytes privateKey = new Bytes(BigIntegers.asUnsignedByteArray(32, key));
+    private String getSdkSignature() {
+        final Bytes privateKey = new Bytes(BigIntegers.asUnsignedByteArray(32, key));
         KeyWallet keyWallet = KeyWallet.load(privateKey);
 
-        BigInteger networkId = new BigInteger("1");
+        final BigInteger networkId = new BigInteger("1");
         final String from = "hx8a2c51300334d7791cf8377ae42b721f537a4e01";
         final String to = "hxb678bd32e86308fdc05bf44fe005c0c935dbaba2";
-        final BigInteger version = BigInteger.valueOf(1);
         final BigInteger value = BigInteger.valueOf(1000);
         final BigInteger nonce = BigInteger.valueOf(1);
         final BigInteger stepLimit = BigInteger.valueOf(100000);
-        BigInteger timestamp = new BigInteger("1708911437891000");
+        final BigInteger timestamp = new BigInteger("1708911437891000");
 
         Transaction transaction = TransactionBuilder.newBuilder()
                 .nid(networkId)
@@ -71,7 +57,46 @@ public class IconTest {
                 .build();
 
         SignedTransaction signedTransaction = new SignedTransaction(transaction, keyWallet);
-        // 2DL341dGihRSQrVPDflLQHQnTvxeQgsjl62Y35LVthdpmyJjXN0/QDW0D+QP4vOqExZucDQS7rcYmOh2kEGuqwA=
-        log.info("signature = {}", signedTransaction.getProperties().getItem("signature"));
+
+        String sdkSignature = signedTransaction.getProperties().getItem("signature").asString();
+        log.info("sdkSignature = {}", sdkSignature);
+        return sdkSignature;
     }
+
+    private String getSignature() {
+        final Bytes privateKey = new Bytes(BigIntegers.asUnsignedByteArray(32, key));
+        ECDSASignature ecdsaSignature = new ECDSASignature(privateKey);
+        byte[] data = new SHA3.Digest256().digest(serializedTransaction.getBytes(StandardCharsets.UTF_8));
+        BigInteger[] sig = ecdsaSignature.generateSignature(data);
+        byte[] signatureBytes = ecdsaSignature.recoverableSerialize(sig, data);
+        String signature = Base64.toBase64String(signatureBytes);
+        log.info("signature = {}", signature);
+        return signature;
+    }
+
+    @Test
+    public void serialize() {
+        final String signature = getSignature();
+
+        // serialize
+        ByteBuffer buffer = ByteBuffer.allocate(serializedTransaction.length() + signature.length());
+        buffer.put(serializedTransaction.getBytes());
+        buffer.put(signature.getBytes());
+        byte[] serializedSignedRawTx = buffer.array();
+
+        // deserialize
+        ByteBuffer slice = ByteBuffer.wrap(serializedSignedRawTx).slice();
+        int size = slice.limit() - serializedTransaction.length();
+        byte[] rawTransaction = new byte[serializedTransaction.length()];
+        byte[] rawSignature = new byte[size];
+        slice.get(rawTransaction);
+        slice.get(rawSignature);
+
+        String deserializedRawTransaction = new String(rawTransaction).trim();
+        String deserializedSignature = new String(rawSignature);
+
+        Assertions.assertThat(deserializedRawTransaction).isEqualTo(serializedTransaction);
+        Assertions.assertThat(deserializedSignature).isEqualTo(signature);
+    }
+
 }
